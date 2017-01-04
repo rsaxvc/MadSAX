1294dc683576df578b6b30adf17c350f94f63561
Richard <rsaxvc@rsaxvc.net>
2017-01-03 22:36:12 -0600

    Provide MADSAX_ API

--- madsax_support.c
+++ madsax_support.c
@@ -1,63 +1,96 @@
-#include <stdio.h>
 #include <stdlib.h>
-
-char * tag_buffer;
-size_t tag_buffer_len;
-size_t tag_buffer_max;
-
-static void tagBufferInit()
-{
-tag_buffer = malloc(1);
-tag_buffer_len = 0;
-tag_buffer_max = 0;
-tag_buffer[tag_buffer_len] = 0;
-}
-
-static void tagBufferDestroy()
-{
-free(tag_buffer);
-tag_buffer = NULL;
-tag_buffer_len = 0;
-}
-
 #include <string.h>
-
-static void tagBufferPush(const char * tag, size_t len)
-{
-if(tag_buffer_len + len > tag_buffer_max )
-	{
-	tag_buffer_max = tag_buffer_len + len;
-	tag_buffer = realloc( tag_buffer, tag_buffer_max + 1);
-	}
-strcpy(tag_buffer+tag_buffer_len,tag);
-tag_buffer_len += len;
-}
-
-static void tagBufferPop(const char * tag, size_t len)
-{
-tag_buffer_len -= len;
-tag_buffer[tag_buffer_len] = 0;
-}
-
 #include <expat.h>
+
+typedef struct
+	{
+	XML_Parser parser;
+	void * userData;
+	char * buffer;
+	size_t buffer_len;
+	size_t buffer_max;
+	}MADSAX_ParserStruct,* MADSAX_Parser;
+
+#define MADSAX_Char XML_Char
+#define MADSAX_SetUserData( p, rValUserData ) (p)->userData = (rValUserData)
+#define MADSAX_Parse(p, b, len, done) XML_Parse((p)->parser, (b), (len), (done))
+#define MADSAX_ParseBuffer(p, len, done) XML_ParseBuffer((p)->parser, (len), (done))
+#define MADSAX_GetBuffer(p, len) XML_GetBuffer((p)->parser, (len)))
 
 const struct tag_handler *
 in_word_set (register const char * str, register unsigned int len);
 
-static void StartElementHandler(void *userData, const XML_Char *name, const XML_Char **atts)
+static void MADSAX_StartElementHandler(void *userData, const XML_Char *tag_name, const XML_Char **atts)
 {
-tagBufferPush("/", 1 );
-tagBufferPush(name, strlen(name) );
-printf("StartElementHandler:%s:%s\n",name,tag_buffer);
-const struct tag_handler * tag = in_word_set( tag_buffer, tag_buffer_len );
-if( tag )tag->tag_start(userData,name,atts);
+MADSAX_Parser p = userData;
+
+/*Insert /tag and reterminate*/
+size_t len = strlen(tag_name);
+size_t len_needed = p->buffer_len + len + 1;/*this +1 is for tag-sep*/
+if( len_needed > p->buffer_max )
+	{
+	p->buffer_max = p->buffer_len + len;
+	void * newBuf = XML_MemRealloc( p->parser, p->buffer, len_needed + 1);/*This one is for NULL*/
+	if( !newBuf )
+		{
+		XML_StopParser( p->parser, XML_FALSE );
+		return;
+		}
+	p->buffer = newBuf;
+	}
+strcpy(p->buffer+p->buffer_len,"/");p->buffer_len += 1;
+strcpy(p->buffer+p->buffer_len,tag_name);p->buffer_len += len;
+
+/*Call start-tag handler*/
+const struct tag_handler * tag = in_word_set( p->buffer, p->buffer_len );
+if( tag )tag->tag_start( p->userData, tag_name, atts );
 }
 
-static void EndElementHandler(void *userData, const XML_Char *name)
+static void MADSAX_EndElementHandler(void *userData, const XML_Char *tag_name)
 {
-printf("EndElementHandler:%s:%s\n",name,tag_buffer);
-const struct tag_handler * tag = in_word_set( tag_buffer, tag_buffer_len );
-if( tag )tag->tag_end(userData,name);
-tagBufferPop(name, strlen(name) );
-tagBufferPop("/", 1 );
+MADSAX_Parser p = userData;
+
+/*Call end-tag handler*/
+const struct tag_handler * tag = in_word_set( p->buffer, p->buffer_len );
+if( tag )tag->tag_end(p->userData,tag_name);
+
+/*Remove /tag by reterminating*/
+p->buffer_len -= strlen(tag_name) + 1;
+p->buffer[p->buffer_len] = 0;
+}
+
+static MADSAX_Parser MADSAX_ParserCreate(const XML_Char * encoding)
+{
+MADSAX_Parser p = malloc(sizeof(MADSAX_ParserStruct));
+if( p )
+	{
+	p->parser = XML_ParserCreate(encoding);
+	if( p->parser )
+		{
+		void * buf = XML_MemMalloc( p->parser, 1 );
+		if( buf )
+			{
+			p->buffer = buf;
+			p->buffer_len = 0;
+			p->buffer_max = 0;
+			p->buffer[p->buffer_len] = 0;
+			XML_SetElementHandler(p->parser, MADSAX_StartElementHandler,MADSAX_EndElementHandler);
+			XML_SetUserData( p->parser, p );
+			return p;
+		}
+		XML_ParserFree( p->parser );
+		}
+	free(p);
+	}
+return NULL;
+}
+
+static void MADSAX_ParserFree( MADSAX_Parser p )
+{
+free(p->buffer);
+p->buffer = NULL;
+p->buffer_len = 0;
+p->buffer_max = 0;
+XML_ParserFree( p->parser );
+free(p);
 }
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "example.c"
 
 #include <stdio.h>
-#include <expat.h>
 
 int main()
 {
@@ -11,47 +10,61 @@ int main()
                   /* depth is thus going to be the user data for this parser. */
 
   FILE * fh = fopen( "example.svg", "r" );
-  tagBufferInit();
-  XML_Parser parser = XML_ParserCreate(NULL);
-  XML_SetUserData(parser, &depth);
-  XML_SetElementHandler(parser, StartElementHandler,EndElementHandler);
-  do {
+  MADSAX_Parser p = MADSAX_ParserCreate(NULL);
+  MADSAX_SetUserData(p, &depth);
+  do
+  {
 	size_t len = fread(buf, 1, sizeof(buf), fh);
 	done = len != sizeof(buf);
-    if (!XML_Parse(parser, buf, len, done)) {
+    if (!MADSAX_Parse(p, buf, len, done))
+    {
       printf("handle parse error\n");
       return 1;
     }
   } while (!done);
-  XML_ParserFree(parser);
-  tagBufferDestroy();
+  MADSAX_ParserFree(p);
   fclose( fh );
   return 0;
 }
 
+static void handle_tag_start(int * depth, const char * function )
+{
+printf("Depth:%i:%s\n",*depth,function);
+(*depth)++;
+}
 
-static void handle_tag_start__svg(void *data, const char *el, const char **attr){}
-static void handle_tag_end__svg(void *data, const char *el){}
+static void handle_tag_end(int * depth, const char * function )
+{
+(*depth)--;
+printf("Depth:%i:%s\n",*depth,function);
+}
+
+/*
+These are individual function calls for tag entry/exit/data.
+These are equivalent to what EXPAT provides, but MADSAX splits
+them up.
+*/
+static void handle_tag_start__svg(void *data, const char *el, const char **attr){handle_tag_start(data,__func__);}
+static void handle_tag_end__svg(void *data, const char *el){handle_tag_end(data,__func__);}
 static void handle_tag_data__svg(void *data, const char *content, int length){}
-static void handle_tag_start__svg__defs(void *data, const char *el, const char **attr){}
-static void handle_tag_end__svg__defs(void *data, const char *el){}
+static void handle_tag_start__svg__defs(void *data, const char *el, const char **attr){handle_tag_start(data,__func__);}
+static void handle_tag_end__svg__defs(void *data, const char *el){handle_tag_end(data,__func__);}
 static void handle_tag_data__svg__defs(void *data, const char *content, int length){}
-static void handle_tag_start__svg__defs__linearGradient(void *data, const char *el, const char **attr){}
-static void handle_tag_end__svg__defs__linearGradient(void *data, const char *el){}
+static void handle_tag_start__svg__defs__linearGradient(void *data, const char *el, const char **attr){handle_tag_start(data,__func__);}
+static void handle_tag_end__svg__defs__linearGradient(void *data, const char *el){handle_tag_end(data,__func__);}
 static void handle_tag_data__svg__defs__linearGradient(void *data, const char *content, int length){}
-static void handle_tag_start__svg__defs__style(void *data, const char *el, const char **attr){}
-static void handle_tag_end__svg__defs__style(void *data, const char *el){}
+static void handle_tag_start__svg__defs__style(void *data, const char *el, const char **attr){handle_tag_start(data,__func__);}
+static void handle_tag_end__svg__defs__style(void *data, const char *el){handle_tag_end(data,__func__);}
 static void handle_tag_data__svg__defs__style(void *data, const char *content, int length){}
-static void handle_tag_start__svg__rect(void *data, const char *el, const char **attr){}
-static void handle_tag_end__svg__rect(void *data, const char *el){}
+static void handle_tag_start__svg__rect(void *data, const char *el, const char **attr){handle_tag_start(data,__func__);}
+static void handle_tag_end__svg__rect(void *data, const char *el){handle_tag_end(data,__func__);}
 static void handle_tag_data__svg__rect(void *data, const char *content, int length){}
-static void handle_tag_start__svg__circle(void *data, const char *el, const char **attr){}
-static void handle_tag_end__svg__circle(void *data, const char *el){}
+static void handle_tag_start__svg__circle(void *data, const char *el, const char **attr){handle_tag_start(data,__func__);}
+static void handle_tag_end__svg__circle(void *data, const char *el){handle_tag_end(data,__func__);}
 static void handle_tag_data__svg__circle(void *data, const char *content, int length){}
-static void handle_tag_start__svg__line(void *data, const char *el, const char **attr){}
-static void handle_tag_end__svg__line(void *data, const char *el){}
+static void handle_tag_start__svg__line(void *data, const char *el, const char **attr){handle_tag_start(data,__func__);}
+static void handle_tag_end__svg__line(void *data, const char *el){handle_tag_end(data,__func__);}
 static void handle_tag_data__svg__line(void *data, const char *content, int length){}
-static void handle_tag_start__svg__polyline(void *data, const char *el, const char **attr){}
-static void handle_tag_end__svg__polyline(void *data, const char *el){}
+static void handle_tag_start__svg__polyline(void *data, const char *el, const char **attr){handle_tag_start(data,__func__);}
+static void handle_tag_end__svg__polyline(void *data, const char *el){handle_tag_end(data,__func__);}
 static void handle_tag_data__svg__polyline(void *data, const char *content, int length){}
-
